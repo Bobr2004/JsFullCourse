5f93aa555c65e7d5d201a31c0d93e8cc5c60f542
bohdan <bohdenshoshka@gmail.com>
2023-10-30 14:58:07 +0200

    finished 2nd project

--- Section2.2/logic.js
+++ Section2.2/logic.js
@@ -1,9 +1,30 @@
-const module = document.querySelector(".module");
-const closeButton = module.querySelector(".close-module");
-
+const module1 = document.querySelector(".module");
+const overlay = document.querySelector(".overlay");
+const closeButton = module1.querySelector(".close-module");
 const cards = document.querySelectorAll(".card");
-console.log(cards);
+
+const changeDisplay = function (parameter) {
+   return function () {
+      if (parameter === "show") {
+         module1.classList.remove("hidden");
+      } else if (parameter === "hide") {
+         module1.classList.add("hidden");
+      }
+   };
+};
 
 for (let i = 0; i < cards.length; i += 1) {
-   cards[i].addEventListener("click");
+   cards[i].addEventListener("click", changeDisplay("show"));
 }
+
+closeButton.addEventListener("click", changeDisplay("hide"));
+overlay.addEventListener("click", changeDisplay("hide"));
+
+document.addEventListener("keydown", function (event) {
+   console.log(event.key);
+
+   if (!module1.classList.contains("hidden") && event.key === "Escape") {
+      console.log("oleg");
+      changeDisplay("hide")();
+   }
+});
